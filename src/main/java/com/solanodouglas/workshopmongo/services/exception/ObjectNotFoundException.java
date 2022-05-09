c8d21b3e9b6ca43c977bd74ea95e9739ac85c0ba
Douglas Moraes <douglasmoraessolano@gmail.com>
2022-05-09 14:58:46 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/solanodouglas/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.solanodouglas.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
